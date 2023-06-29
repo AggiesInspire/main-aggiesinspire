41e5b3dd1ca2fd3fe2d58c537559b9a0a11faa4f
jozhw <j_zhw@proton.me>
2023-06-29 00:05:14 -0500

    add-contactus: contact page

---
+++ src/app/contact/ContactSection.tsx
@@ -0,0 +1,9 @@
+import { ContactUs } from "@/components/ui/sections/ContactUs";
+
+export default function ContactSection() {
+  return (
+    <div>
+      <ContactUs />
+    </div>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import ContactSection from "@/app/contact/ContactSection";
+import { PageWrapper } from "@/components/PageWrapper";
+
+export const metadata: Metadata = {
+  title: "Aggies Inspire | Contact Us",
+  description: "",
+};
+
+export default function Contact() {
+  return (
+    <section id="contact" className="mt-20 bg-gray-20 md:h-full md:pb-0">
+      <PageWrapper>
+        <ContactSection />
+      </PageWrapper>
+    </section>
+  );
+}
