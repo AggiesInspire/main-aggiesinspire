8ba2dcd3e6f02fbb3ee17700e581676b23a6a866
jozhw <j_zhw@proton.me>
2023-07-04 20:35:05 -0500

    update-homepartners: add new partner and change bg to animate-gradient via x

--- src/app/(home)/HomePartnersSection.tsx
+++ src/app/(home)/HomePartnersSection.tsx
@@ -1,17 +1,41 @@
 "use client";
 import React from "react";
-import PartnerCCF from "@/images/PartnerCCF.png";
+import PartnerCCF from "@/images/partners/CCFLogo.png";
+import PartnerTheBaseChicago from "@/images/partners/TheBaseChicagoLogo.png";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import Image from "next/image";
 
 const HomeSponsorSection = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+
+  const partners = [
+    {
+      image: (
+        <a href="https://www.thebasechi.org/" target="_blank">
+          <Image alt="ccf-partner" src={PartnerTheBaseChicago} height="80" />
+        </a>
+      ),
+    },
+    {
+      image: (
+        <a href="https://chicagochessfoundation.org/" target="_blank">
+          <Image alt="ccf-partner" src={PartnerCCF} height="75" />
+        </a>
+      ),
+    },
+  ];
   return (
     <section id="home-partners">
       {/* Sponsors */}
       <div>
         {isAboveMediumScreens && (
-          <div className="h-[150px] w-full bg-primary-100 pt-10 pb-20">
+          <div
+            className="h-[150px] w-full pt-10 pb-20 bg-gradient-to-r
+          from-primary-400
+          to-primary-100
+          via-gray-200
+          animate-gradient-x"
+          >
             <div className="mx-auto w-11/12">
               <div className="flex w-3/5 items-center justify-between gap-5">
                 <a
@@ -25,9 +49,9 @@ const HomeSponsorSection = () => {
                     height="79"
                   />
                 </a>
-                <a href="https://chicagochessfoundation.org/" target="_blank">
-                  <Image alt="ccf-partner" src={PartnerCCF} height="75" />
-                </a>
+                {partners.map((partner) => (
+                  <div>{partner.image} </div>
+                ))}
               </div>
             </div>
           </div>
