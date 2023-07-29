d53dc498e11c38119b4ed2a0257af75e35f596dd
jozhw <j_zhw@proton.me>
2023-07-28 22:30:39 -0500

    fixed-syntax: frameborder to frameBorder (deprecated)

--- src/app/(home)/HomeScreen.tsx
+++ src/app/(home)/HomeScreen.tsx
@@ -36,9 +36,8 @@ const HomeSection = () => {
               style={{ width: "300%", height: "100%", marginLeft: "-100%" }}
               src="https://www.youtube.com/embed/lftcPmaELss?autoplay=1&mute=1&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&playlist=lftcPmaELss"
               title="YouTube video player"
-              frameborder="0"
+              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowfullscreen
             ></iframe>
           </div>
           <div
