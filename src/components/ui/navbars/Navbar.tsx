8ffc4a9079a9fecdde2c02fb12de0ed26130d258
jozhw <j_zhw@proton.me>
2023-07-28 22:13:29 -0500

    update-navbar: logo change on scroll

--- src/components/ui/navbars/Navbar.tsx
+++ src/components/ui/navbars/Navbar.tsx
@@ -13,43 +13,44 @@ import { SelectedPage } from "@/components/types";
 import ButtonLink from "@/components/links/ButtonLink";
 import { buttonVariants } from "../Button";
 
-
 type Props = {
   navbarLinks?: any;
   miniNavbarLinks?: any;
   appearScroll: number;
   onTop?: boolean;
-}
-const Navbar = ({navbarLinks, miniNavbarLinks, appearScroll, onTop }: Props)  => {
+};
+const Navbar = ({
+  navbarLinks,
+  miniNavbarLinks,
+  appearScroll,
+  onTop,
+}: Props) => {
   useEffect(() => {
-      window.addEventListener("unload", checkOnLoad);
-      return () => window.removeEventListener("unload", checkOnLoad);
-      }, []);
+    window.addEventListener("unload", checkOnLoad);
+    return () => window.removeEventListener("unload", checkOnLoad);
+  }, []);
 
-  
   const checkOnLoad = () => {
-    console.log("beforeunload")
-      const onLoad = () => {
-    console.log(window.scrollY);
-    if (window.scrollY <= appearScroll) {
-      setScrolled(false);
-    } else {
-      setScrolled(true);
-    }
-  };
-  if (typeof window != "undefined") {
-    window.onload = () => {
-      /* call onLoad function on refresh and remove the refresh session so that it can be repeated */
-      var reloading = sessionStorage.getItem("reloading");
-      if (reloading) {
-        sessionStorage.removeItem("reloading");
-        onLoad();
+    console.log("beforeunload");
+    const onLoad = () => {
+      console.log(window.scrollY);
+      if (window.scrollY <= appearScroll) {
+        setScrolled(false);
+      } else {
+        setScrolled(true);
       }
     };
-  }
-
-
-  }
+    if (typeof window != "undefined") {
+      window.onload = () => {
+        /* call onLoad function on refresh and remove the refresh session so that it can be repeated */
+        var reloading = sessionStorage.getItem("reloading");
+        if (reloading) {
+          sessionStorage.removeItem("reloading");
+          onLoad();
+        }
+      };
+    }
+  };
   // /* makes sure that if the page is refreshed the navbar style is correct based on scrollY position */
   const flexBetween = "flex items-center justify-between";
 
@@ -76,71 +77,65 @@ const Navbar = ({navbarLinks, miniNavbarLinks, appearScroll, onTop }: Props)  =>
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
-
   return (
     <>
-        <motion.div
-          className="fixed h-1 bg-gray-500 top-0 left-0 right-0 origin-left z-50"
-          style={{ scaleX }}
-        />
+      <motion.div
+        className="fixed h-1 bg-gray-500 top-0 left-0 right-0 origin-left z-50"
+        style={{ scaleX }}
+      />
 
-        { scrolled || onTop  ? (
+      {scrolled || onTop ? (
+        <motion.nav
+          className={`bg-primary-100 backdrop-filter backdrop-blur-lg bg-opacity-80 fixed z-10 w-full py-6 h-20 ${flexBetween}`}
+        >
+          <div className={`${flexBetween}  mx-auto w-11/12`}>
+            <div className={`${flexBetween} w-full gap-16`}>
+              {/* Left Side */}
+              <Link href={SelectedPage.Home}>
+                {scrolled ? (
+                  <Image alt="logo" src={Logo2} width={80} height={80} />
+                ) : (
+                  <Image alt="logo" src={Logo} width={350} height={200} />
+                )}
+              </Link>
 
+              {/* Right Side */}
+              {isAboveMediumScreens ? (
+                <div className={`${flexBetween} w-full`}>
+                  <div className={`${flexBetween} gap-8 text-sm`}></div>
+                  <div className={`${flexBetween} gap-8 text-sm`}>
+                    {/* map out navbarlinks on md above screens */}
+                    {navbarLinks
+                      ? navbarLinks.map((link: any) => (
+                          <Link
+                            href={link.link}
+                            className={buttonVariants({ variant: "ghost" })}
+                            key={link.title}
+                          >
+                            {link.title}
+                          </Link>
+                        ))
+                      : ""}
 
-            <motion.nav
-            className={
-             `bg-primary-100 backdrop-filter backdrop-blur-lg bg-opacity-80 fixed z-10 w-full py-6 h-20 ${flexBetween}`
-            }
-            >
-        <div className={`${flexBetween}  mx-auto w-11/12`}>
-          <div className={`${flexBetween} w-full gap-16`}>
-            {/* Left Side */}
-            <Link href={SelectedPage.Home}>
-                <Image
-                  alt="logo"
-                  src={Logo}
-                  priority={true}
-                  width={350}
-                  height={200}
-                />
-            </Link>
-
-            {/* Right Side */}
-            {isAboveMediumScreens ? (
-              <div className={`${flexBetween} w-full`}>
-                <div className={`${flexBetween} gap-8 text-sm`}></div>
-                <div className={`${flexBetween} gap-8 text-sm`}>
-                  {/* map out navbarlinks on md above screens */}
-                  { navbarLinks ? navbarLinks.map((link: any) =>
-                    (<Link
-                      href={link.link}
-                      className={
-                           buttonVariants({ variant: "ghost" })
-                      }
-                      key={link.title}
-                    >
-                      {link.title}
-                    </Link>)):
-                    ""}
-
-
-                  <ButtonLink path={SelectedPage.Contact}>
-                    <p className="text-lg">Contact Us</p>
-                  </ButtonLink>
+                    <ButtonLink path={SelectedPage.Contact}>
+                      <p className="text-lg">Contact Us</p>
+                    </ButtonLink>
+                  </div>
                 </div>
-              </div>
-            ) : (
-              <button
-                className=" rounded-full bg-secondary-500 p-2"
-                onClick={() => setIsMenuToggled(!isMenuToggled)}
-              >
-                <Bars3Icon className="h-6 w-6 text-white" />
-              </button>
-            )}
+              ) : (
+                <button
+                  className=" rounded-full bg-secondary-500 p-2"
+                  onClick={() => setIsMenuToggled(!isMenuToggled)}
+                >
+                  <Bars3Icon className="h-6 w-6 text-white" />
+                </button>
+              )}
+            </div>
           </div>
-        </div>
         </motion.nav>
-        ) : (<div></div>) }
+      ) : (
+        <div></div>
+      )}
 
       {/* Mobile Menu Mode */}
       {!isAboveMediumScreens && isMenuToggled && (
