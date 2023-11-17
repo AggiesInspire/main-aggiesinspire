42c7c7b140216b323ddd06ee09298f001eebb7c0
John W <112655410+jozhw@users.noreply.github.com>
2023-11-17 16:26:33 -0600

    V0.2.0 (#5)
    * fix: learn about us button navigation
    
    * update: board descriptions

--- src/objects/images/boardMembers.tsx
+++ src/objects/images/boardMembers.tsx
@@ -12,27 +12,27 @@ export const boardMembers = {
     title: "Vice President",
     name: "Joseph Esquivel",
     description:
-      "Mr. Esquivel actively serves as the Vice President of Aggies inspire Inc. and is one of our founders. He was raised in a Costa Rican home and lived most of his life on Texas soil. As a young adult, he was obsessed with robotics and later worked in the engineering department at Lockheed Martin. Today, the Costa Rican is a senior at Texas A&M studying Logistics who will be working in operations research at Applied Materials Inc.",
+      "Mr. Esquivel serves as the Vice President of Aggies inspire Inc. and is one of our founders. He was raised in a Costa Rican home and lived most of his life on Texas soil. As a young adult, he was obsessed with robotics and later worked in the engineering department at Lockheed Martin. Today, the Costa Rican is graduate from Texas A&M University with a BBA in Logistics who is working in operations research at Applied Materials Inc.",
   },
   secretary: {
     url: "https://res.cloudinary.com/main-aggiesinspire/image/upload/v1697030905/board/Peter_vy5qqf.jpg",
     title: "Secretary",
     name: "Peter Wu",
     description:
-      "Mr. Wu currently serves as the Secretary for Aggies Inspire Inc. and is one of our founders. Mr. Wu is a native to College Station. He has experience in private equity conducting due diligence to place financial firms in a strong strategic positions. He is a senior Texas A&M student majoring in Finance.",
+      "Mr. Wu currently serves as the Secretary for Aggies Inspire Inc. and is one of our founders. Mr. Wu is a native to College Station. He has experience in private equity conducting due diligence to place financial firms in a strong strategic positions. He is a graduate from Texas A&M University with a BBA in Finance.",
   },
   treasurer: {
     url: "https://res.cloudinary.com/main-aggiesinspire/image/upload/v1697030902/board/Grace_mc7hv6.jpg",
     title: "Treasurer",
     name: "Grace Wu",
     description:
-      "Ms. Wu currently serves as the Treasurer of Aggies Inspire. Ms. Wu joined the board on October 2021. She is a native to College Station and brings in work experience in tax-accounting at one of the big four accounting firms. She is a junior Texas A&M student in the PPA program majoring in Accounting.",
+      "Ms. Wu currently serves as the Treasurer of Aggies Inspire. Ms. Wu joined the board on October 2021. She is a native to College Station and brings in work experience in tax-accounting at one of the big four accounting firms. She is pursing a masters in Accounting at Texas A&M University.",
   },
   firstDirector: {
     url: "https://res.cloudinary.com/main-aggiesinspire/image/upload/v1697030903/board/Jonathan_dvxbgt.jpg",
     title: "Director",
     name: "Jonathan Childs",
     description:
-      "Mr. Childs currently serves as a board member and the Chief Student Leader of Aggies Inspire Club at Texas A&M University. Mr. Childs joined the board on March 2023. He is a College Station native, who is current senior at Texas A&M University pursuing a BBA in Finance. While at Texas A&M, he has utilized the foundational knowledge imparted to gain a deeper understanding of fiscal social behavior.",
+      "Mr. Childs currently serves as a board member. Mr. Childs joined the board on March 2023. He is a College Station native, who graduated from Texas A&M University with a BBA in Finance. While at Texas A&M, he has utilized the foundational knowledge imparted to gain a deeper understanding of fiscal social behavior.",
   },
 };
